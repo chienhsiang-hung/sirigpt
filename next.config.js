d42d8ef2a0976e1f7f66f685b7b74bb7cf9832b0
chienhsiang-hung <77676044+chienhsiang-hung@users.noreply.github.com>
2023-02-22 13:43:59 +0800

    fix img problem

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  images : {
+    domains: ['chienhsiang-hung.github.io']
+  },
 
   async headers() {
     return [
